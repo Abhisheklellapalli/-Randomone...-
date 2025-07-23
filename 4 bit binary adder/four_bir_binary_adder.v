8b4e723d5f7e7bab808d2df7ef99c666e8f1b084
ABHISHEK LELLAPALLI <23P35A0409@ACET.AC.IN>
2025-07-23 22:25:32 +0530

    Update four_bir_binary_adder.v

--- 4 bit binary adder/four_bir_binary_adder.v
+++ 4 bit binary adder/four_bir_binary_adder.v
@@ -1,3 +1,4 @@
+// Code your design here
 module four_bit_binary_adder(
     input [3:0] a,
     input [3:0] b,
@@ -5,7 +6,7 @@ module four_bit_binary_adder(
     output [3:0] sum,
     output cout
     );
-    wire [2:1]c;
+  wire [3:1]c;
     assign sum[0]=a[0]^b[0]^cin;
     assign c[1]=a[0]&b[0]|b[0]&cin|cin&a[0];
     assign sum[1]=a[1]^b[1]^c[1];
